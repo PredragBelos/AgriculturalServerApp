a8c18f26f85ecee6d6fc7ab20e2f189adef8ab8b
PredragBelos <predragbelos@gmail.com>
2023-09-15 09:55:09 +0200

    Connect with database on limited_rs

--- src/config/database.configuration.ts
+++ src/config/database.configuration.ts
@@ -1,7 +1,7 @@
 export const DatabaseConfiguration = {
-    hostname: "localhost",
-    username: "aplikacija",
+    hostname: "185.119.89.160",
+    username: "predragb_admin",
     password: "0205989840003Peki",
-    database: "agricultural_app",
+    database: "predragb_agricultural_holding",
     port: 3306
 }
